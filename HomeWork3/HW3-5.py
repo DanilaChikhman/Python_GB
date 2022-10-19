a7dd1cb5dbd11a58e621b131eca0b3cea16c3825
DanilaChikhman <danila.chikhman2015@gmail.com>
2022-10-19 17:38:50 +0600

    HW3-5

--- HomeWork3/HW3-5.py
+++ HomeWork3/HW3-5.py
@@ -2,7 +2,19 @@
 # Пример:
 # - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] 
 
-s = int(input('введите целое число ')) 
-# print(s)
-i = 0
-arr =[]
+n = int(input('Введите число: '))
+
+def get_fibonacci(n):
+    fibo_nums = []
+    a, b = 1, 1
+    for i in range(n):
+        fibo_nums.append(a)
+        a, b = b, a + b
+    a, b = 0, 1
+    for i in range (n+1):
+        fibo_nums.insert(0, a)
+        a, b = b, a - b
+    return fibo_nums
+
+fibo_nums = get_fibonacci(n)
+print(get_fibonacci(n))

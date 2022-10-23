c91b4a8f6982bc099c7a4d0c993fdcdb42973258
DanilaChikhman <danila.chikhman2015@gmail.com>
2022-10-23 15:21:34 +0600

    HW4

---
+++ HomeWork4/HW4-3.py
@@ -0,0 +1,19 @@
+# Задайте последовательность цифр. Напишите программу, которая выведет список неповторяющихся элементов
+# исходной последовательности.
+# Пример:
+# 47756688399943 -> [5]
+# 1113384455229 -> [8,9]
+# 1115566773322 -> []
+
+
+numbers = list(map(int, input("Введите числа через пробел:\n").split()))
+def get_unique_numbers(numbers):
+    list_of_unique_numbers = []
+    unique_numbers = set(numbers)
+
+    for number in unique_numbers:
+        list_of_unique_numbers.append(number)
+
+    return list_of_unique_numbers
+
+print(get_unique_numbers(numbers))
